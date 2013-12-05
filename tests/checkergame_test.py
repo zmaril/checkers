d3392f41c04b8cbf11a6a4a4d9a90d5473de7df1
Zack <zack@zacharymaril.com>
2013-12-05 16:18:37 +0000

    Added in some simple tests for checkergame.

--- tests/checkergame_test.py
+++ tests/checkergame_test.py
@@ -1,8 +1,8 @@
 import unittest 
-from checkergame import partitions2checkers,checkers2partitions,rtest
+from checkergame import rtest,run
 
 class TestCheckergame(unittest.TestCase):
-    def test_partitions2checkers(self):
+    def test_rtest(self):
         #These tests are from the matrix in
         #"SOLVING SCHUBERT PROBLEMS"
 
@@ -21,8 +21,8 @@ class TestCheckergame(unittest.TestCase):
         self.assertEqual(rtest(0,2,1,1,[99,99,1],3),([99,99,1],0))
         self.assertEqual(rtest(0,1,1,0,[99,1],2),([99,1],0))
 
-        #self.assertEqual(partitions2checkers(6,15,[1,1,1]),
-#                         [9,10,11,13,14,15]) 
-#Wrong but something weird is going on.
-#
+    def test_run(self):
+        self.assertEqual(run([99, 99, 99, 3, 99, 5], [99, 99, 99, 3, 99, 5]),([[99, 99, 99, 3, 4, 99], [99, 99, 2, 99, 99, 5]], {1: [2], 2: [0, 0]}))
+        self.assertEqual(run([0, 99, 99, 99, 4, 5, 99, 7], [99, 99, 99, 3, 99, 5, 6, 7]),([[0, 99, 99, 99, 4, 5, 6, 99], [0, 99, 99, 3, 99, 5, 99, 7]], {1: [2], 2: [0, 0]}))
+        self.assertEqual(run([0, 1, 99, 3, 99, 99], [99, 99, 2, 99, 4, 5]),([[0, 1, 2, 99, 99, 99]], {1: [0]}))
     
