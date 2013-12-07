85a841f56639a35d65339c1adbf95f9c588068c8
Zack <zack@zacharymaril.com>
2013-12-07 14:18:16 +0000

    Added in timing code for testing all the problems with k,n. The benchmarks on my computer looked like this, where the tuple is (k,n,seconds to computer): (3, 3, 1.2874603271484375e-05) (3, 4, 0.0007729530334472656) (3, 5, 3.4375200271606445) (4, 4, 1.0013580322265625e-05) (4, 5, 0.0028030872344970703) (4, 6, 24960.645797014236)

---
+++ src/timer.py
@@ -0,0 +1,15 @@
+from timeit import timeit
+from time import sleep
+import sys
+
+def benchmark():
+    for k in range(3,5):
+        for n in range(k,k+3):
+            p = "test_all_problems_G("+str(k)+","+str(n)+")"
+            t = timeit(p,number=1,setup="from go import test_all_problems_G")
+            print(k,n,t)
+
+    for i in range(1,10):
+        sys.stdout.write('\a')
+        sys.stdout.flush()
+        sleep(0.5)
