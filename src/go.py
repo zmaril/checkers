6237d7a1dff05f9baf449a8d6612f0f0b4fdc20b
Aaron Moore <mooreaarond@gmail.com>
2014-05-01 14:02:56 -0500

    Created output for G(4,9) and made a frmatted version for frank

--- src/go.py
+++ src/go.py
@@ -7,7 +7,7 @@ def outer_gg(sch_prob):
   n = len(sch_prob[1])
   gg = GG2(Key(n,parts=sch_prob))
   if gg.alternating is False:
-    return (sch_prob,gg)
+    return (sch_prob,gg.solutions)
 
 def get_problems(k,n,m):
   boxes = k*(n-k)
@@ -32,6 +32,26 @@ def test_all_problems_G(k,n, db=0):
   failures = []
   #m is the number of conditions
   for m in range(3, boxes+1):
-    failures.append(filter(None,pool.map(outer_gg,get_problems(k,n,m))))
-  return failures
+    results = filter(None,pool.map(outer_gg,get_problems(k,n,m)))
+    if results != []:  
+      failures.extend(results)
 
+  return filter(smallerGrassmannianFilter, failures)
+
+#filter to remove all problems which exist in a smaller grassmannian
+#failure[0] is a schubert problem
+def smallerGrassmannianFilter(failure):
+  for condition in failure[0]:
+    if condition[0] == 0 or condition[-1] == 99:
+      return False
+  return True
+  
+
+def saveToFile(k,n,fileName):
+  f = open(fileName, 'w')
+  results = test_all_problems_G(k,n)
+  for r in results:
+    f.write(str(r))
+    f.write('\n')
+    #f.write(str(r[0]) + ' = ' + str(r[1]) + '\n')
+  f.close()
